43a267f26086f21038863a49cadcb750930d0a30
rnovick13 <rnovick13@gmail.com>
2020-01-10 13:20:05 -0500

    fetch header state set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+const API = `http://localhost:3000/books`
+
+class App extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+      .then(response => response.json())
+      .then(books => this.setState({books: books}))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Book Tracker</h1>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
